3e08ca4d4e03329accfd52cedf1a1cf66c24b455
Rafael Silveira <rafael.silveira@syntesis.com.br>
2021-03-30 22:29:23 -0300

    description: - Addition another index to customize with material-ui

---
+++ src/Application/index-default.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react'
+import ListItems from '../Components/ListItems'
+import useInput from '../Hooks/useInput'
+
+import './styles.css'
+
+function Application() {
+  const [id, setId] = useState(3)
+  const name = useInput()
+  const [listCount, setListCount] = useState(3)
+  const [listNames, setListNames] = useState(() => {
+    const initialState = [
+      { id: 1, title: 'Aprender React', completed: false },
+      { id: 2, title: 'Aprender Html e Javascript', completed: true }
+    ]
+
+    return initialState
+  })
+
+  useEffect(() => {
+    console.warn('Documento foi iniciado.')
+    document.title = 'Lista de Tarefas'
+  }, [])
+
+  useEffect(() => {
+    setListCount(listNames.length)
+  }, [listNames])
+
+  // Functions
+  const handleAdd = () => {
+    const {
+      value,
+      onChange
+    } = name
+
+    const newItem = {
+      id: id + 1,
+      title: value,
+      completed: false
+    }
+    if (value) {
+      setListNames([...listNames, newItem])
+      onChange()
+      setId(id + 1)
+    } else {
+      alert('Não é possível adicionar um item vazio!')
+    }
+  }
+
+  const handleDelete = (id) => {
+    const newItems = listNames.filter(item => item.id !== id)
+
+    setListNames(newItems)
+  }
+
+  const handleComplete = (id) => {
+    const newItems = listNames.map(item => {
+      if (item.id === id) {
+        item.completed = true
+      }
+      return item
+    })
+
+    setListNames(newItems)
+  }
+
+  return (
+    <div className="content">
+      <h1 className="title">Minha Lista de Tarefas</h1>
+      <input className="nameInput" type="text" placeholder="Insira sua tarefa aqui" {...name} />
+      <button className="button" onClick={handleAdd}>Adicionar</button>
+      <ul className="list">
+        {
+          listNames.map((item) => {
+            return (
+              <ListItems
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+                handleComplete={handleComplete}
+              />
+            )
+          })
+        }
+      </ul>
+      <label className="count">N° de Registros: {listCount}</label>
+    </div>
+  )
+}
+
+export default Application
